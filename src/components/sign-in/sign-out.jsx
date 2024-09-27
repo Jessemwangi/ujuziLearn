f0cb488b21c277c2f49591a88196d6a4329c41f0
JESSE <jessejzee@gmail.com>
2024-09-27 14:56:56 +0300

    delete token

--- src/components/sign-in/sign-out.jsx
+++ src/components/sign-in/sign-out.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setError, setToken, setUser } from '../../redux/features/loginSlice';
+import { TokenService } from "../../services/token";
 
 const SignOut = () => {
     const dispatch = useDispatch()
     dispatch(setError(null))
     dispatch(setUser(null));
     dispatch(setToken(null));
+    TokenService.deleteToken()
     return (
         <div>
             
