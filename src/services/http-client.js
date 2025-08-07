51b9a55ddf95e733be8ee47d19398a1e08cd792d
JESSE <jessejzee@gmail.com>
2025-08-07 12:59:52 +0300

    fix static token

--- src/services/http-client.js
+++ src/services/http-client.js
@@ -14,7 +14,7 @@ const httpClient = axios.create({
 
 httpClient.interceptors.request.use(
   (config) => {
-    const token = `cf2a59fa219393f01b6ce8c9d2c9bff281e17e41433a20006132d1e8e9af2465e641804d5db404af485865de99bee3da13cb048f1803e86b71ed9fd71c1eb8201ebccff8dee2b23e3e5b12041d52afc3bda6bb975c303c5813760f3d03b5ba2ea8eb80f6d0dff06b2c8803a7a3ca35451f1ff73331ee477f0d3566e4da6d7c4e`;
+    const token = TokenService.getToken();
     if (token) {
       if (config.headers) {
         if (!config.headers["Authorization"]) {
